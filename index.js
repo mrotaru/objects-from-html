b76d3fb6bfde9db049c55e9523b0bae7eb4dd268
Mihai Rotaru <mihai.rotaru@gmx.com>
2019-07-04 07:49:44 +0100

    wip: use generators

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-const objectsFromHtml = (html, descriptor) => {
-    return []
+const cheerio = require("cheerio");
+
+function* objectsFromHtml(html, itemDescriptor, context = {}) {
+  // TODO: validate descriptor
+
+  const $ = cheerio.load(html, {
+    normalizeWhitespace: false,
+    xmlMode: false,
+    decodeEntities: true
+  });
+
+  const { container, selector } = itemDescriptor;
+  let $items = container ? $(container).find(selector) : $(selector);
+
+  $items.each((i, $element) => {
+    let item = {};
+    for (let propertyKey of Object.keys(itemDescriptor.properties)) {
+      const propertyDescriptor = itemDescriptor.properties[propertyKey];
+      if (typeof propertyDescriptor === "string") {
+        let $propertyElement = $($element).find(propertyDescriptor);
+        item[propertyKey] = $propertyElement.text();
+      }
+    }
+    yield item
+  });
 }
 
-module.exports = objectsFromHtml
+module.exports = objectsFromHtml;
