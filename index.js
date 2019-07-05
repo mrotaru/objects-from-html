20c9a2eff3dbec23dc29f2cc713b397f705154de
Mihai Rotaru <mihai.rotaru@gmx.com>
2019-07-05 07:13:45 +0100

    rm generators, use std fns

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const cheerio = require("cheerio");
+const assert = require("assert");
 
-function* objectsFromHtml(html, itemDescriptor, context = {}) {
+function objectsFromHtml(html, itemDescriptor, context = {}) {
   // TODO: validate descriptor
 
   const $ = cheerio.load(html, {
@@ -12,17 +13,33 @@ function* objectsFromHtml(html, itemDescriptor, context = {}) {
   const { container, selector } = itemDescriptor;
   let $items = container ? $(container).find(selector) : $(selector);
 
+  const items = [];
   $items.each((i, $element) => {
     let item = {};
     for (let propertyKey of Object.keys(itemDescriptor.properties)) {
       const propertyDescriptor = itemDescriptor.properties[propertyKey];
       if (typeof propertyDescriptor === "string") {
+        // just a CSS selector
         let $propertyElement = $($element).find(propertyDescriptor);
         item[propertyKey] = $propertyElement.text();
+      } else if (typeof propertyDescriptor === "object") {
+        // CSS selector and an 'extract' property
+        assert(propertyDescriptor.selector, "must have selector");
+        assert(propertyDescriptor.extract, "must have extract");
+        const { selector, extract } = propertyDescriptor;
+        let $propertyElement = $($element).find(selector);
+        if (extract === "text") {
+          item[propertyKey] = $propertyElement.text();
+        } else if (extract === "href") {
+          item[propertyKey] = $propertyElement.attr("href");
+        } else if (extract === "html") {
+          item[propertyKey] = $propertyElement.parent().html();
+        }
       }
     }
-    yield item
+    items.push(item);
   });
+  return items;
 }
 
 module.exports = objectsFromHtml;
--- index.test.js
+++ index.test.js
@@ -1,10 +1,9 @@
-const test = require('tape')
+const test = require("tape");
 
-const objectsFromHtml = require('.')
+const objectsFromHtml = require(".");
 
-test('smoke test', t => {
-    const html=
-`<div class='books'>
+test("smoke test", t => {
+  const html = `<div class='books'>
     <div class='book'>
         <h3 class='title'>Foo</h3>
         <div class='rating'>4/5</div>
@@ -13,26 +12,95 @@ test('smoke test', t => {
         <h3 class='title'>Bar</h3>
         <div class='rating'>3/5</div>
     </div>
-</div>`
-    const descriptor = {
-        "name": "book",
-        "container": ".books",
-        "selector": ".book",
-        "properties": {
-            "title": ".title",
-            "rating": ".rating"
-        }
+</div>`;
+  const descriptor = {
+    name: "book",
+    selector: ".book",
+    properties: {
+      title: ".title",
+      rating: ".rating"
     }
+  };
 
-    const result = objectsFromHtml(html, descriptor)
+  const result = objectsFromHtml(html, descriptor);
 
-    t.deepEqual(result, [{
-        title: "Foo",
-        rating: "4/5",
-    }, {
-        title: "Bar",
-        rating: "3/5",
-    }])
+  t.deepEqual(result, [
+    {
+      title: "Foo",
+      rating: "4/5"
+    },
+    {
+      title: "Bar",
+      rating: "3/5"
+    }
+  ]);
 
-    t.end()
-})
+  t.end();
+});
+
+test("extract text", t => {
+  const html = "<div><p>Some <strong>nice</strong> text</p></div>";
+  const descriptor = {
+    name: "foo",
+    selector: "div",
+    properties: {
+      allText: {
+        selector: "p",
+        extract: "text"
+      }
+    }
+  };
+  const result = objectsFromHtml(html, descriptor);
+  t.deepEqual(result, [
+    {
+      allText: "Some nice text"
+    }
+  ]);
+
+  t.end();
+});
+
+test("extract href", t => {
+  const html =
+    '<div><p>This is a link to <a href="http://foo.com">Foo</a>, which is nice.</p></div>';
+  const descriptor = {
+    name: "foo",
+    selector: "div",
+    properties: {
+      allText: {
+        selector: "a",
+        extract: "href"
+      }
+    }
+  };
+  const result = objectsFromHtml(html, descriptor);
+  t.deepEqual(result, [
+    {
+      allText: "http://foo.com"
+    }
+  ]);
+
+  t.end();
+});
+
+test("extract html", t => {
+  const html = "<div><p>Some <strong>nice</strong> text</p></div>";
+  const descriptor = {
+    name: "foo",
+    selector: "div",
+    properties: {
+      allText: {
+        selector: "p",
+        extract: "html"
+      }
+    }
+  };
+  const result = objectsFromHtml(html, descriptor);
+  t.deepEqual(result, [
+    {
+      allText: "<p>Some <strong>nice</strong> text</p>"
+    }
+  ]);
+
+  t.end();
+});
